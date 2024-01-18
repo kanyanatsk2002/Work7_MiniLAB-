fb9b69062864bf384f9ee2ce4af73d65699f298c
Kanyanat S <79918423+kanyanatsk2002@users.noreply.github.com>
2024-01-18 14:34:42 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="header">
+      <header>
+        <h1>Resume</h1>
+        <h2>Miss Kanyanat Saenkaew</h2>
+      </header>
+      <body className="body">
+        <p>สีที่ชอบ : ชมพู</p>
+        <p>ความสูง : 161 cm.</p>
+        <p>น้ำหนัก : 44 kg.</p>
+        <p>คำคมที่ชอบ : Live a happy life alone♡.</p>
+      </body>
+    </div>
+  );
+}
+
+export default App;
